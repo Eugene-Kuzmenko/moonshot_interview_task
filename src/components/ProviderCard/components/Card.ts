abaf3ca71fbf5f486514f9e788eaece87752a63c
Eugene-Kuzmenko <e.kuzmenko@dunice.net>
2022-07-01 11:18:24 +0300

    Add some styled components

---
+++ src/components/ProviderCard/components/Card.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+  padding: 16px 0 12px 16px;
+  width: 100%;
+  background-color: #FFFFFF;
+  border-radius: 12px;
+`;
+
+export default Card;
