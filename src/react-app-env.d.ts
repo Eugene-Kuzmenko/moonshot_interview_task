66a96e9a64836e934f329c82ca1af8dd8c0b6e8a
Eugene-Kuzmenko <e.kuzmenko@dunice.net>
2022-07-01 11:16:44 +0300

    Add provider typings

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+
+declare module 'assets/providers.json' {
+  import { ProviderCardData } from './types/provider';
+
+  declare const data: ProviderCardData[];
+
+  export default data;
+}
---
+++ src/types/provider.ts
@@ -0,0 +1,61 @@
+
+export interface ImageData {
+  src: string,
+  alt: string,
+}
+
+export interface LinkData {
+  ling: string,
+  text: string,
+}
+
+export interface ProviderLogo {
+  hideNumber: true,
+  logo: ImageData,
+  logoMobile: ImageData,
+  useAltLogoForMobile: boolean,
+}
+
+export interface ProviderRatingPageData {
+  path: string,
+  title: string,
+  id: 0,
+}
+
+export interface ProviderRating {
+  value: number,
+  linkText: string,
+  page: ProviderRatingPageData,
+  votes: number,
+  votesFollowText: string,
+}
+
+export interface ProviderBonus {
+  offerTitle: string,
+  offerSubText: string,
+}
+
+export interface ProviderScore {
+  score: number,
+  progressBar: boolean,
+  animation: boolean,
+}
+
+export interface ImageWithTextData {
+  image: ImageData,
+  text: string,
+}
+
+export interface ProviderCardData {
+  providerName: string,
+  providerDisclaimerHiderForDesktop: boolean,
+  providerLogo: ProviderLogo,
+  providerRating: ProviderRating,
+  minDeposit: string,
+  minDepositLabel: string,
+  providerBonus: ProviderBonus,
+  providerScore: ProviderScore,
+  providerVisitLink: LinkData,
+  providerLegalDisclaimer: string,
+  slotsImages: ImageWithTextData,
+}
